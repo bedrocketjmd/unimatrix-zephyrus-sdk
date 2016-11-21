a5ba48a447666fcfe55e8cd077264a9b35a3a4f6
asherkory <asher.kory@gmail.com>
2016-11-21 10:31:01 -0500

    Set up structure and gemspec

---
+++ lib/zephyrus.rb
@@ -0,0 +1,24 @@
+require 'active_support'
+require 'active_support/all'
+require 'fnv'
+
+require 'zephyrus/configuration'
+require 'zephyrus/response'
+require 'zephyrus/request'
+require 'zephyrus/parser'
+require 'zephyrus/serializer'
+require 'zephyrus/operation'
+
+require 'zephyrus/resources/base'
+
+# Errors
+require 'zephyrus/resources/error'
+require 'zephyrus/resources/forbidden_error'
+require 'zephyrus/resources/attribute_error'
+require 'zephyrus/resources/missing_parameter_error'
+require 'zephyrus/resources/attribute_error'
+require 'zephyrus/resources/malformed_parameter_error'
+require 'zephyrus/resources/not_found_error'
+
+# Resources
+require 'zephrys/resources/input'

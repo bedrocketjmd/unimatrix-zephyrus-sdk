d3956577bd5e4ca31e58786cfd0906fa05901d11
Asher Kory <asher.kory@gmail.com>
2017-07-24 10:42:42 -0400

    Change Transcoding Rendition class name

--- lib/zephyrus/resources/transcoding_output.rb
+++ lib/zephyrus/resources/transcoding_output.rb
@@ -1,17 +1,9 @@
-
 module Zephyrus
   class TranscodingOutput < Output
 
-    field :content_type
-    field :video_bitrate
-    field :audio_bitrate
-    field :height
-    field :width
-    field :audio_sample_rate
     field :segment_duration
     field :segment_count
-    field :profile_idc
-    field :level_idc
+    field :content_type
 
   end
 end
